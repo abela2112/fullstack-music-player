19c5a9a7c6b1280551046c59865a49f007e659b8
Abel Ayalew Abebe <103965027+abela2112@users.noreply.github.com>
2023-10-17 22:58:18 +0300

    Update Player.jsx

--- client/src/components/MusicPlayer/Player.jsx
+++ client/src/components/MusicPlayer/Player.jsx
@@ -22,7 +22,7 @@ const Player = ({ activeSong, isPlaying, volume, seekTime, onEnded, onTimeUpdate
 
   return (
     <audio
-      src={`${import.meta.env.VITE_BASE_URL}${activeSong?.song}`}
+      src={`${import.meta.env.VITE_BASE_URL}/${activeSong?.song}`}
       ref={ref}
       loop={repeat}
       onEnded={onEnded}
